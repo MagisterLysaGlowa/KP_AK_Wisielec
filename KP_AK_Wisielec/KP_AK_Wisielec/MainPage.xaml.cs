1320e5c5e5f6ec1982fbb9bae54db7fb9c42cee5
aleksander.klak <akaczka800@gmail.com>
2024-03-05 12:05:39 +0100

    Added categories list for picker_categories

--- KP_AK_Wisielec/KP_AK_Wisielec/MainPage.xaml.cs
+++ KP_AK_Wisielec/KP_AK_Wisielec/MainPage.xaml.cs
@@ -10,9 +10,16 @@ namespace KP_AK_Wisielec
 {
     public partial class MainPage : ContentPage
     {
+        List<string> categories = new List<string>()
+        {
+            "item", "animal", "city", "plant"
+        };
+
         public MainPage()
         {
             InitializeComponent();
+            picker_category.ItemsSource = categories;
+            picker_category.SelectedItem = categories[0];
         }
     }
 }
