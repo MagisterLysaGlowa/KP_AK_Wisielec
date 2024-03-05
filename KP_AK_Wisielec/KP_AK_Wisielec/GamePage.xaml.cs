7ed13fb51688b398c0de6e413adcfd94217cc7e7
Kacper Piaskowy <kacperpiaskowy937@gmail.com>
2024-03-05 12:02:34 +0100

    Add important variables and objects

--- KP_AK_Wisielec/KP_AK_Wisielec/GamePage.xaml.cs
+++ KP_AK_Wisielec/KP_AK_Wisielec/GamePage.xaml.cs
@@ -12,9 +12,15 @@ namespace KP_AK_Wisielec
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GamePage : ContentPage
     {
-        public GamePage()
+        string button_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-.";
+        string password_guess = "";
+        string hidden_password = "";
+        string category_name = "";
+        int mistakes = 0;
+        public GamePage(string category_name)
         {
             InitializeComponent();
+            this.category_name = category_name;
         }
     }
 }
